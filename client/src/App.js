6283109df7ea17da70f4aa4b531cb194580cb7a1
Dalton <duong.daltonle@gmail.com>
2019-02-01 14:32:12 +1100

    Add media queries for adaptive app

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.scss'
 import { Provider } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
+import Media from 'react-media'
 import store from './state/store'
 import Dashboard_desktop from './views/pages/Dashboard/Dashboard_desktop'
 import Dashboard_mobile from './views/pages/Dashboard/Dashboard_mobile';
@@ -11,9 +12,19 @@ class App extends Component {
     return (
       <Provider store={ store }>
         <div className="App">
-          <Switch>
-            <Route exact path='/' component={Dashboard_desktop} />
-          </Switch>
+          <Media query="(max-width: 599px)">
+            { matches =>
+              matches ? (
+                <Switch>
+                  <Route exact path='/' component={Dashboard_mobile} />
+                </Switch>
+              ) : (
+                <Switch>
+                  <Route exact path='/' component={Dashboard_desktop} />
+                </Switch>
+              )
+            }
+          </Media>
         </div>
       </Provider>
     )
