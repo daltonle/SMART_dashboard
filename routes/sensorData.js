f27dd68a01f57022fda33b90b8d229eea1f4e981
Dalton <duong.daltonle@gmail.com>
2019-02-07 02:03:16 +1100

    Update API endpoint

--- routes/sensorData.js
+++ routes/sensorData.js
@@ -11,8 +11,8 @@ router.get('/air/:id', (req, res, next) => {
   let query = {
     text: `SELECT pm2_5, pm10, to_char(ts, 'DD-MM-YYYY HH24:mm:ss') FROM aq_data
       WHERE id_aq=$1::text
-      ORDER BY ts
-      LIMIT 200`,
+      ORDER BY ts DESC
+      LIMIT 500`,
     values: [req.params.id]
   }
 
@@ -21,18 +21,94 @@ router.get('/air/:id', (req, res, next) => {
     .catch(next)
 })
 
+// retrieve air quality data of a sensor based on coordinates
+router.get('/air/:long,:lat', (req, res, next) => {
+  let {
+    long,
+    lat
+  } = req.params
+
+  let query = {
+    text: `SELECT pm2_5, pm10, to_char(ts, 'DD-MM-YYYY HH24:mm:ss') FROM aq_data
+    WHERE aq_sensor.id=aq_data.id AND aq_sensor.long=$1::numeric AND aq_sensor.lat=$2::numeric
+    ORDER BY aq_data.ts DESC
+    LIMIT BY 500`,
+    values: [long, lat]
+  }
+  
+  db.query(query)
+    .then(result => res.json(result.rows))
+    .catch(next)
+})
+
+// retrieve live data of air sensor based on coordinates
+router.get('/air/live/:long,:lat', (req, res, next) => {
+  let {
+    long,
+    lat
+  } = req.params
+
+  let query = {
+    text: `SELECT pm2_5, pm10, to_char(ts, 'DD-MM-YYYY HH24:mm:ss') FROM aq_sensor
+    WHERE aq_sensor.long=$1::numeric AND aq_sensor.lat=$2::numeric`,
+    values: [long, lat]
+  }
+  
+  db.query(query)
+    .then(result => res.json(result.rows[0]))
+    .catch(next)
+})
+
 // retrieve visual data of a sensor
 router.get('/visual/:id', (req, res, next) => {
   let query = {
-    text: `SELECT to_char(ts, 'DD-MM-YYYY HH24:mm:ss'), type, count FROM vs_count
+    text: `SELECT to_char(ts, 'DD-MM-YYYY HH24:mm:ss'), type, counter FROM vs_count
       WHERE id_vs=$1::text
-      ORDER BY ts
-      LIMIT 200`,
+      ORDER BY ts DESC
+      LIMIT 500`,
     values: [req.params.id]
   }
 
   db.query(query)
     .then(result => res.json(result.rows))
+    .catch(next)
+})
+
+// retrieve visual data history of a sensor based on coordinates
+router.get('/visual/:long,:lat', (req, res, next) => {
+  let {
+    long,
+    lat
+  } = req.params
+
+  let query = {
+    text: `SELECT to_char(ts, 'DD-MM-YYYY HH24:mm:ss'), type, counter FROM vs_count
+    WHERE vs_count.id=vs_sensor.id AND vs_sensor.long=$1::numeric AND vs_sensor.lat=$2::numeric
+    ORDER BY vs_count.ts DESC
+    LIMIT BY 500`,
+    values: [long, lat]
+  }
+  
+  db.query(query)
+    .then(result => res.json(result.rows))
+    .catch(next)
+})
+
+// retrieve live visual data of a sensor based on coordinates
+router.get('/visual/live/:long,:lat', (req, res, next) => {
+  let {
+    long,
+    lat
+  } = req.params
+
+  let query = {
+    text: `SELECT to_char(ts, 'DD-MM-YYYY HH24:mm:ss'), pedestrians, vehicles, bicycles FROM vs_sensor
+    WHERE vs_sensor.long=$1::numeric AND vs_sensor.lat=$2::numeric`,
+    values: [long, lat]
+  }
+  
+  db.query(query)
+    .then(result => res.json(result.rows[0]))
     .catch(next)
 })
 
