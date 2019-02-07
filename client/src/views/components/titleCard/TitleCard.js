f640887e07969e4495c96cb5a8ff3453dc85dd2e
Dalton <duong.daltonle@gmail.com>
2019-02-07 13:11:39 +1100

    Add TitleCard component

---
+++ client/src/views/components/titleCard/TitleCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import MapPinIcon from 'react-feather/dist/icons/map-pin'
+import CompassIcon from 'react-feather/dist/icons/compass'
+import styles from './TitleCard.module.scss'
+
+export const TitleCard = (props) => {
+  let {
+    name,
+    suburb,
+    position
+  } = props
+  
+  return (
+    <div>
+      <h1>{name}</h1>
+      <div className={styles.info}>
+        <MapPinIcon className={styles.icon} />
+        <h5>{suburb}</h5>
+      </div>
+      <div className={styles.info}>
+        <CompassIcon className={styles.icon} />
+        <h5>{`${position.long}, ${position.lat}`}</h5>
+      </div>
+    </div>
+  )
+}
