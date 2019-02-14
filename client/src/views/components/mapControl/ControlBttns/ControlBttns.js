905701cfd5daa25edfe56cf07d07575297dff8e4
Dalton <duong.daltonle@gmail.com>
2019-02-15 10:10:05 +1100

    Adapt map ControlBttns for mobile

--- client/src/views/components/mapControl/ControlBttns/ControlBttns.js
+++ client/src/views/components/mapControl/ControlBttns/ControlBttns.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { DESK, MOBILE } from '../../../../utils/const'
 import InfoIcon from 'react-feather/dist/icons/info'
 import LayersIcon from 'react-feather/dist/icons/layers'
 import CompareIcon from 'react-feather/dist/icons/bar-chart-2'
@@ -9,29 +10,53 @@ import styles from './.module.scss'
  * Control buttons used for desktop map
  */
 
-export const LegendsBttn = () => (
-  <button className={styles.bttn} data-tip data-for='legends'>
-    <InfoIcon className={styles.icon} />
-    <ReactTooltip id='legends' place="left" type="light" effect="solid">
-      <span>Legends</span>
-    </ReactTooltip>
-  </button>
-)
+export const LegendsBttn = ({ media }) => {
+  if (media === DESK)
+    return (
+      <button className={styles.bttn} data-tip data-for='legends'>
+        <InfoIcon className={styles.icon} />
+        <ReactTooltip id='legends' place="left" type="light" effect="solid">
+          <span>Legends</span>
+        </ReactTooltip>
+      </button>
+    )
+  else return (
+    <button className={styles.bttn} data-tip data-for='legends'>
+      <InfoIcon className={styles.icon} />
+    </button>
+  )
+}
 
-export const LayersBttn = () => (
-  <button className={styles.bttn} data-tip data-for='layers'>
-    <LayersIcon className={styles.icon} />
-    <ReactTooltip id='layers' place="left" type="light" effect="solid">
-      <span>Switch layer</span>
-    </ReactTooltip>
-  </button>
-)
+export const LayersBttn = ({ media }) => {
+  if (media === DESK)
+    return (
+      <button className={styles.bttn} data-tip data-for='layers'>
+        <LayersIcon className={styles.icon} />
+        <ReactTooltip id='layers' place="left" type="light" effect="solid">
+          <span>Switch layer</span>
+        </ReactTooltip>
+      </button>
+    )
+  else return (
+    <button className={styles.bttn} data-tip data-for='layers'>
+      <LayersIcon className={styles.icon} />
+    </button>
+  )
+}
 
-export const CompareBttn = () => (
-  <button className={styles.bttn} data-tip data-for='compare'>
-    <CompareIcon className={styles.icon} />
-    <ReactTooltip id='compare' place="left" type="light" effect="solid">
-      <span>Compare</span>
-    </ReactTooltip>
-  </button>
-)
+export const CompareBttn = ({ media }) => {
+  if (media === DESK)
+    return (
+      <button className={styles.bttn} data-tip data-for='compare'>
+        <CompareIcon className={styles.icon} />
+        <ReactTooltip id='compare' place="left" type="light" effect="solid">
+          <span>Compare</span>
+        </ReactTooltip>
+      </button>
+    )
+  else return (
+    <button className={styles.bttn} data-tip data-for='compare'>
+      <CompareIcon className={styles.icon} />
+    </button>
+  )
+}
