21d901742402e093127527890dd9aa6b3c9cb403
Dalton <duong.daltonle@gmail.com>
2019-02-15 01:17:36 +1100

    Update API to retrieve data by hour and day of week

--- routes/sensorData.js
+++ routes/sensorData.js
@@ -12,7 +12,7 @@ router.get('/air/:id', (req, res, next) => {
     text: `SELECT pm2_5, pm10, to_char(ts, 'DD-MM-YYYY HH24:MI:SS') as timestamp FROM aq_data
       WHERE id_aq=$1::text
       ORDER BY ts DESC
-      LIMIT 5000`,
+      LIMIT 100`,
     values: [req.params.id]
   }
 
@@ -113,7 +113,7 @@ router.get('/visual/live/:long,:lat', (req, res, next) => {
 })
 
 // retrieve air quality data from a time period
-router.get('/air/:id/:year-:month-:day', (req, res, next) => {
+router.get('/air/by-time/:id/:year-:month-:day', (req, res, next) => {
   let {
     year,
     month,
@@ -138,7 +138,7 @@ router.get('/air/:id/:year-:month-:day', (req, res, next) => {
 })
 
 // retrieve visual data from a time period
-router.get('/visual/:id/:year-:month-:day', (req, res, next) => {
+router.get('/visual/by-time/:id/:year-:month-:day', (req, res, next) => {
   let {
     year,
     month,
@@ -151,12 +151,48 @@ router.get('/visual/:id/:year-:month-:day', (req, res, next) => {
   let d_start = moment(date).subtract(15, 'days')
 
   let query = {
-    text: `SELECT to_char(ts, 'DD-MM-YYYY HH24:MI:SS'), type, count FROM vs_count
+    text: `SELECT to_char(ts, 'DD-MM-YYYY HH24:MI:SS'), type, counter FROM vs_count
       WHERE id_vs = $1::text AND '[${d_start.format('YYYY-MM-DD')}, ${d_end.format('YYYY-MM-DD')}]'::daterange @> ts::date`,
     values: [id]
   }
 
   console.log(query)
+  db.query(query)
+    .then(result => res.json(result.rows))
+    .catch(next)
+})
+
+// retrieve average air quality by hour
+router.get('/air/by-hour/:id', (req, res, next) => {
+  let query = {
+    text: `SELECT extract(dow from ts) as dow,
+                  extract(hour from ts) as hour,
+                  avg(pm2_5) as pm2_5,
+                  avg(pm10) as pm10
+          FROM aq_data
+          WHERE id_aq = $1
+          GROUP BY 1,2`,
+    values: [req.params.id]
+  }
+
+  db.query(query)
+    .then(result => res.json(result.rows))
+    .catch(next)
+})
+
+// retrieve average vehicle count by hour
+router.get('/visual/by-hour/:id', (req, res, next) => {
+  let query = {
+    text: `SELECT extract(dow from ts) as dow,
+                  extract(hour from ts) as hour,
+                  type,
+                  avg(counter) as counter
+          FROM vs_count
+          WHERE id_vs = $1
+          GROUP BY 1,2,3`,
+    values: [req.params.id]
+  }
+
   db.query(query)
     .then(result => res.json(result.rows))
     .catch(next)
