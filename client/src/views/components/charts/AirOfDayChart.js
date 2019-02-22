eccac23012b2a51c7f06dc163dd958c316f2fd96
Dalton <duong.daltonle@gmail.com>
2019-02-22 13:25:33 +1100

    Mobile UI for chart details

--- client/src/views/components/charts/AirOfDayChart.js
+++ client/src/views/components/charts/AirOfDayChart.js
@@ -6,11 +6,13 @@ import DatePicker from 'react-datepicker'
 import moment from 'moment'
 import 'react-datepicker/dist/react-datepicker-cssmodules.css'
 import withDimension from 'react-dimensions'
+import { MOBILE } from '../../../utils/const'
 import { colors } from '../../../styles/colors'
 import styles from './AirOfDayChart.module.scss'
 
 class AirOfDayChart extends Component {
   static propTypes = {
+    media: PropTypes.string,
     sensor: PropTypes.object
   }
 
@@ -19,10 +21,6 @@ class AirOfDayChart extends Component {
     this.state = {
       selectedDate: new Date()
     }
-  }
-  
-  componentDidMount = () => {
-    
   }
 
   handleDateChange = (date) => {
@@ -55,13 +53,69 @@ class AirOfDayChart extends Component {
     }
   }
 
+  onDatepickerRef = (el) => {
+    if (el && el.input) { el.input.readOnly = true }
+  }
+
   render() {
-    const { containerHeight, containerWidth } = this.props
-    const dataPM2_5 = this.getData("pm2_5")
-    const dataPM10 = this.getData("pm10")
+    const { containerHeight, containerWidth, media } = this.props
+    
+    const data = [
+      {
+        ...this.getData("pm2_5"),
+        name: "PM2_5",
+        type: 'scatter',
+        mode: 'markers',
+        marker: { color: colors.green, opacity: 0.7 }
+      },
+      {
+        ...this.getData("pm10"),
+        name: "PM10",
+        type: "scatter",
+        mode: "markers",
+        marker: { color: colors.red, opacity: 0.7 }
+      }
+    ]
+
+    // chart layout
+    const webLayout = { 
+      width: containerWidth, 
+      height: containerHeight,
+      showlegend: true,
+      legend: { x: 0, y: 1.25, orientation: "h" },
+      plot_bgcolor: colors.backgroundColor,
+      paper_bgcolor: colors.backgroundColor,
+      margin: {
+        t: 0,
+        pad: 8
+      }
+    }
+    
+    const mobileLayout = {
+      ...webLayout,
+      margin: {
+        t: 100,
+        l: 40,
+        r: 0,
+        b: 64,
+        pad: 8
+      }
+    }
+
+    // chart config
+    const webConfig = {
+      modeBarButtonsToRemove: ['toImage', 'sendDataToCloud', 'select2d', 'lasso2d', 'toggleSpikelines'],
+      displaylogo: false,
+      displayModeBar: 'hover'
+    }
+
+    const mobileCongig = {
+      ...webConfig,
+      displayModeBar: true
+    }
 
     return (
-      <div>
+      <div className={styles.container}>
         <div className={styles.options}>
           <h5>Choose a date</h5>
           <DatePicker
@@ -69,43 +123,13 @@ class AirOfDayChart extends Component {
             selected={this.state.selectedDate}
             onChange={date => this.handleDateChange(date)}
             className={styles.datepicker}
+            ref={el => this.onDatepickerRef(el)}
           />
         </div>
         <Plot
-          data={[
-            {
-              x: dataPM2_5.x,
-              y: dataPM2_5.y,
-              name: "PM2_5",
-              type: 'scatter',
-              mode: 'markers',
-              marker: { color: colors.green, opacity: 0.7 }
-            },
-            {
-              x: dataPM10.x,
-              y: dataPM10.y,
-              name: "PM10",
-              type: "scatter",
-              mode: "markers",
-              marker: { color: colors.red, opacity: 0.7 }
-            }
-          ]}
-          config={{
-            modeBarButtonsToRemove: ['toImage', 'sendDataToCloud', 'select2d', 'lasso2d', 'toggleSpikelines'],
-            displaylogo: false,
-          }}
-          layout={{ 
-            width: containerWidth, 
-            height: containerHeight,
-            showlegend: true,
-            legend: { x: 0, y: 1.25, orientation: "h" },
-            plot_bgcolor: colors.backgroundColor,
-            paper_bgcolor: colors.backgroundColor,
-            margin: {
-              t: 0,
-              pad: 8
-            }
-          }}
+          data={data}
+          config={media===MOBILE ? mobileCongig : webConfig}
+          layout={media===MOBILE ? mobileLayout : webLayout}
         />
       </div>
     )
